d3c7a481e27b64010f144ac8a47b97b8b1a44f86
björn munthe <bjomun1011@skola.goteborg.se>
2021-02-04 19:55:18 +0100

    First version of ouf birbgame

--- App.java
+++ App.java
@@ -1,5 +1,22 @@
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+/**
+ * This is a very small "game" just to show the absolute basics of
+ * how to draw on a surface in a frame using Swing/AWT.
+ * 
+ */
 public class App {
     public static void main(String[] args) {
-        System.out.print("Jumpy birb")
+        JFrame main = new JFrame("Invaders");
+
+        GameSurface gs = new GameSurface(400, 400);
+
+        main.setSize(400, 400);
+        main.setResizable(false);
+        main.add(gs);
+        main.addKeyListener(gs);
+        main.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        main.setVisible(true);
     }
 }
---
+++ GameSurface.java
@@ -0,0 +1,159 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+/**
+ * A simple panel with a space invaders "game" in it. This is just to
+ * demonstrate the bare minimum of stuff than can be done drawing on a panel.
+ * This is by no means very good code.
+ * 
+ */
+public class GameSurface extends JPanel implements ActionListener, KeyListener {
+    private static final long serialVersionUID = 6260582674762246325L;
+
+    private boolean gameOver;
+    private Timer timer;
+    private List<Rectangle> aliens;
+    private Rectangle spaceShip;
+
+    public GameSurface(final int width, final int height) {
+        this.gameOver = false;
+        this.aliens = new ArrayList<>();
+
+
+            addAlien(30, 150);
+
+
+        this.spaceShip = new Rectangle(80, width / 2 - 15, 30, 20);
+
+        this.timer = new Timer(20, this);
+        this.timer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        repaint(g);
+
+    }
+
+    private void addAlien(final int width, final int height) {
+        int x = 250;
+        int y = 100;
+        aliens.add(new Rectangle(380, 250, 30, 150));
+        aliens.add(new Rectangle(380, 0, 30, 150));
+    }
+
+    /**
+     * Call this method when the graphics needs to be repainted on the graphics
+     * surface.
+     * 
+     * @param g the graphics to paint on
+     */
+    private void repaint(Graphics g) {
+        final Dimension d = this.getSize();
+
+        if (spaceShip.y < (this.getSize().height - spaceShip.height - 10)){
+            spaceShip.translate(0, 1);
+        }
+        if (gameOver) {
+            g.setColor(Color.red);
+            g.fillRect(0, 0, d.width, d.height);
+            g.setColor(Color.black);
+            g.setFont(new Font("Arial", Font.BOLD, 48));
+            g.drawString("Game over!", 20, d.width / 2 - 24);
+            return;
+        }
+
+        // fill the background
+        g.setColor(Color.cyan);
+        g.fillRect(0, 0, d.width, d.height);
+
+        // draw the aliens
+        for (Rectangle alien : aliens) {
+            g.setColor(Color.red);
+            g.fillRect(alien.x, alien.y, alien.width, alien.height);
+        }
+
+        // draw the space ship
+        g.setColor(Color.black);
+        g.fillRect(spaceShip.x, spaceShip.y, spaceShip.width, spaceShip.height);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // this will trigger on the timer event
+        // if the game is not over yet it will
+        // update the positions of all aliens
+        // and check for collision with the space ship
+
+        if (gameOver) {
+            timer.stop();
+            return;
+        }
+
+        final List<Rectangle> toRemove = new ArrayList<>();
+
+        for (Rectangle alien : aliens) {
+            alien.translate(-1, 0);
+            if (alien.x < 10) {
+                // we add to another list and remove later
+                // to avoid concurrent modification in a for-each loop
+                toRemove.add(alien);
+            }
+
+            if (alien.intersects(spaceShip)) {
+                gameOver = true;
+            }
+        }
+
+        aliens.removeAll(toRemove);
+
+        // add new aliens for every one that was removed
+        for (int i = 0; i < toRemove.size(); ++i) {
+            addAlien(30, 150);
+        }
+
+        this.repaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // this event triggers when we release a key and then
+        // we will move the space ship if the game is not over yet
+
+        if (gameOver) {
+            return;
+        }
+
+        final int minHeight = 10;
+        final int maxHeight = this.getSize().height - spaceShip.height - 10;
+        final int kc = e.getKeyCode();
+
+        if (kc == KeyEvent.VK_SPACE && spaceShip.y > minHeight) {
+            spaceShip.translate(0, -30);
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // do nothing
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // do nothing
+    }
+}
