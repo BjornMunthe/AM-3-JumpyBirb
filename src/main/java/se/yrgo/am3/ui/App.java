9e35e35916b87d95b8bc12f92bcedbc171ee0b75
Staffan <staffan.godman@gmail.com>
2021-02-09 16:15:47 +0100

    Created basic JFrame in App

--- src/main/java/se/yrgo/am3/ui/App.java
+++ src/main/java/se/yrgo/am3/ui/App.java
@@ -1,10 +1,21 @@
 package se.yrgo.am3.ui;
 
+import se.yrgo.am3.gameobjects.*;
+import javax.swing.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			JFrame main = new JFrame("Jumpybirb");
 
+			GameSurface gs = new GameSurface(1200, 800);
+
+			main.setSize(1200, 800);
+			main.setResizable(false);
+			main.add(gs);
+			main.setDefaultCloseOperation(EXIT_ON_CLOSE);
+			main.setVisible(true);
 	}
 
 }
