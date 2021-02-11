b7cb2c78e6d407cb3d1773f2caea0abd81f12767
björn munthe <bjomun1011@skola.goteborg.se>
2021-02-11 20:42:22 +0100

    playable version rand position + points

--- src/main/java/se/yrgo/am3/gameobjects/GameSurface.java
+++ src/main/java/se/yrgo/am3/gameobjects/GameSurface.java
@@ -3,13 +3,16 @@ package se.yrgo.am3.gameobjects;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 import javax.swing.*;
 
-public class GameSurface extends JPanel implements ActionListener {
+public class GameSurface extends JPanel implements ActionListener, KeyListener {
     private static final long serialVersionUID = 6260582674762246325L;
 
     private Timer timer;
@@ -20,31 +23,51 @@ public class GameSurface extends JPanel implements ActionListener {
     private List<Pipe> pipes;
     private final Rectangle birb;
     private boolean gameOver;
+    private int points = 0;
+    private int size;
+    private final int SCREEN_WIDTH;
+    private final int SCREEN_HEIGHT;
+    private final int PIPE_WIDTH;
+    private final int PIPE_HEIGHT;
+    private final int PIPE_GAP;
 
     public GameSurface(final int width, final int height) {
         this.timer = new Timer(20, this);
         this.timer.start();
         this.gameOver = false;
+        this.SCREEN_WIDTH = width;
+        this.SCREEN_HEIGHT = height;
+        this.PIPE_WIDTH = SCREEN_WIDTH/8;
+        this.PIPE_HEIGHT = 4*PIPE_WIDTH;
+        this.PIPE_GAP = height/5;
 
        addPipes();
-       this.birb = new Rectangle(300, width / 2 - 15, 30, 20);
+       this.birb = new Rectangle(SCREEN_WIDTH / 2, height / 4, 30, 20);
 
+    }
+
+    private int calculateBottomY() {
+        int temp = 0;
+        while(temp <= PIPE_GAP+50 || temp >= SCREEN_HEIGHT-PIPE_GAP) {
+            temp = ThreadLocalRandom.current().nextInt((int) (SCREEN_HEIGHT / 2 * 1.25));
+        }
+        return temp;
     }
 
     public void addPipes() {
         pipes = new ArrayList<>();
-        pipes.add(firstPipe = new Pipe(100, 400));
-        firstPipe.setxLoc(900);
-        firstPipe.setyLoc(400);
-        pipes.add(secondPipe = new Pipe(100, 200));
-        secondPipe.setxLoc (900);
-        secondPipe.setyLoc(0);
-            pipes.add(thirdPipe = new Pipe(0,0));
-            thirdPipe.setxLoc(0);
-            thirdPipe.setyLoc(0);
-            pipes.add(fourthPipe = new Pipe(0,0));
-            fourthPipe.setxLoc(0);
-            fourthPipe.setyLoc(0);
+        pipes.add(firstPipe = new Pipe(PIPE_WIDTH, PIPE_HEIGHT));
+        firstPipe.setxLoc(SCREEN_WIDTH);
+        firstPipe.setyLoc(calculateBottomY());
+        pipes.add(secondPipe = new Pipe(PIPE_WIDTH, PIPE_HEIGHT));
+        secondPipe.setxLoc(SCREEN_WIDTH);
+        secondPipe.setyLoc(firstPipe.getyLoc() - PIPE_GAP - PIPE_HEIGHT);
+        pipes.add(thirdPipe = new Pipe(0,0));
+        thirdPipe.setxLoc(0);
+        thirdPipe.setyLoc(0);
+        pipes.add(fourthPipe = new Pipe(0,0));
+        fourthPipe.setxLoc(0);
+        fourthPipe.setyLoc(0);
 
     }
 
@@ -62,19 +85,22 @@ public class GameSurface extends JPanel implements ActionListener {
         g.setColor(Color.yellow);
         g.fillRect(birb.x, birb.y, birb.width, birb.height);
 
-
-
         for (Pipe pipe : pipes) {
             g.setColor(Color.green);
             g.fillRect(pipe.getxLoc(), pipe.getyLoc(), pipe.getWidth(), pipe.getHeight());
         }
 
+        g.setColor(Color.red);
+        g.setFont(new Font("Arial", Font.BOLD, 48));
+        g.drawString(String.valueOf(points), d.height / 2, d.width / 4);
+
         if (gameOver) {
-            g.setColor(Color.red);
+           g.setColor(Color.red);
                 g.fillRect(0, 0, d.width, d.height);
                 g.setColor(Color.black);
                 g.setFont(new Font("Arial", Font.BOLD, 48));
-                g.drawString("Game over!", 20, d.width / 2 - 24);
+                g.drawString("Game over!", d.height /2, d.width / 2 - 24);
+                g.drawString("Points" + points, d.height / 2, d.width / 2 + 10);
                 return;
             }
 
@@ -86,31 +112,77 @@ public class GameSurface extends JPanel implements ActionListener {
         // this is where we update positions of different elements in the game
         // this is where we set it to game over
         for (Pipe pipe: pipes) {
-            pipe.setxLoc(pipe.getxLoc() - 1);
+            pipe.setxLoc(pipe.getxLoc() - 2);
             if(pipe.getRectangle().intersects(birb)) {
                 gameOver = true;
             }
         }
+        if(birb.y > 650) {
+            gameOver = true;
+        }
 
-
+        if (birb.y < (this.getSize().height - birb.height - 10)){
+            birb.translate(0, 3);
+        }
 
         if (firstPipe.getxLoc() == 400) {
-            thirdPipe.setItAll(900, 400, 400, 100);
-            fourthPipe.setItAll(900, 0, 200, 100);
+            thirdPipe.setItAll(SCREEN_WIDTH, calculateBottomY(), PIPE_HEIGHT , PIPE_WIDTH);
+            fourthPipe.setItAll(SCREEN_WIDTH, thirdPipe.getyLoc() - PIPE_GAP - PIPE_HEIGHT, PIPE_HEIGHT, PIPE_WIDTH );
         }
 
         if (firstPipe.getxLoc() == -100) {
-            firstPipe.setxLoc(900);
-            secondPipe.setxLoc(900);
+            firstPipe.setxLoc(SCREEN_WIDTH);
+            secondPipe.setxLoc(SCREEN_WIDTH);
+            firstPipe.setyLoc(calculateBottomY());
+            secondPipe.setyLoc(firstPipe.getyLoc() - PIPE_GAP - PIPE_HEIGHT);
         }
 
         if (thirdPipe.getxLoc() == -100) {
-            thirdPipe.setxLoc(900);
-            fourthPipe.setxLoc(900);
+            thirdPipe.setxLoc(SCREEN_WIDTH);
+            fourthPipe.setxLoc(SCREEN_WIDTH);
+            thirdPipe.setyLoc(calculateBottomY());
+            fourthPipe.setyLoc(thirdPipe.getyLoc() - PIPE_GAP - PIPE_HEIGHT);
         }
 
-
-
+        if (firstPipe.getxLoc() == SCREEN_WIDTH / 2 +10 || thirdPipe.getxLoc() == SCREEN_WIDTH / 2 +10) {
+            points++;
+        }
         this.repaint();
     }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // this event triggers when we release a key and then
+        // we will move the space ship if the game is not over yet
+
+        if (gameOver) {
+            return;
+        }
+
+        final int minHeight = 10;
+        final int maxHeight = this.getSize().height - birb.height - 10;
+        final int kc = e.getKeyCode();
+
+        if (kc == KeyEvent.VK_SPACE && birb.y > minHeight) {
+            birb.translate(0, -30);
+        }
+        if (kc == KeyEvent.VK_SPACE && gameOver == true) {
+            gameOver = false;
+            this.repaint();
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // do nothing
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // do nothing
+    }
 }
+
+
+
+
