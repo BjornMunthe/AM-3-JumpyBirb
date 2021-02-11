e640470bd4e39284ebe387f53fd96522dc9338ce
björn munthe <bjomun1011@skola.goteborg.se>
2021-02-11 14:10:21 +0100

    adding pipe class and gamesurface with pipes

--- src/main/java/se/yrgo/am3/gameobjects/GameSurface.java
+++ src/main/java/se/yrgo/am3/gameobjects/GameSurface.java
@@ -6,18 +6,27 @@ import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 import javax.swing.*;
 
 public class GameSurface extends JPanel implements ActionListener {
     private static final long serialVersionUID = 6260582674762246325L;
 
     private Timer timer;
+    private Pipe firstPipe;
+    private Pipe secondPipe;
 
     public GameSurface(final int width, final int height) {
         this.timer = new Timer(20, this);
         this.timer.start();
-    }
 
+        firstPipe = new Pipe(100, 400);
+        firstPipe.setxLoc(900);
+        firstPipe.setyLoc(400);
+        secondPipe = new Pipe(100, 200);
+        secondPipe.setxLoc (900);
+        secondPipe.setyLoc(0);
+    }
     @Override
     protected void paintComponent(Graphics g) {
         repaint(g);
@@ -28,6 +37,15 @@ public class GameSurface extends JPanel implements ActionListener {
         final Dimension d = this.getSize();
         g.setColor(Color.cyan);
         g.fillRect(0, 0, d.width, d.height);
+
+
+        //firstPipe.getRectangle();
+        g.setColor(Color.green);
+        g.fillRect(firstPipe.getxLoc(),firstPipe.getyLoc(),firstPipe.getWidth(), firstPipe.getHeight());
+
+        g.setColor(Color.green);
+        g.fillRect(secondPipe.getxLoc(),secondPipe.getyLoc(), secondPipe.getWidth(), secondPipe.getHeight());
+
     }
 
     @Override
@@ -35,6 +53,13 @@ public class GameSurface extends JPanel implements ActionListener {
         // this will trigger on the timer event
         // this is where we update positions of different elements in the game
         // this is where we set it to game over
+
+        firstPipe.setxLoc(firstPipe.getxLoc() - 1);
+//        firstPipe.setyLoc(400);
+
+        secondPipe.setxLoc(secondPipe.getxLoc() - 1);
+//        secondPipe.setyLoc(0);
+
 
         this.repaint();
     }
---
+++ src/main/java/se/yrgo/am3/gameobjects/Pipe.java
@@ -0,0 +1,49 @@
+package se.yrgo.am3.gameobjects;
+
+import java.awt.*;
+
+public class Pipe {
+    private int xLoc, yLoc;
+    private int width, height;
+
+    public Pipe(int initialWidth, int initialHeight) {
+        this.width = initialWidth;
+        this.height = initialHeight;
+    }
+
+    public int getxLoc() {
+        return xLoc;
+    }
+
+    public void setxLoc(int xLoc) {
+        this.xLoc = xLoc;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getyLoc() {
+        return yLoc;
+    }
+
+    public void setyLoc(int yLoc) {
+        this.yLoc = yLoc;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public Rectangle getRectangle() {
+        return (new Rectangle(xLoc, yLoc, width, height));
+    }
+}
