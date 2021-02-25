89f4083fcf6614d415da5dd197ccd5a70316cd17
björn munthe <bjomun1011@skola.goteborg.se>
2021-02-25 15:51:29 +0100

    change in pipehandling function

--- src/main/java/se/yrgo/am3/gameobjects/GameSurface.java
+++ src/main/java/se/yrgo/am3/gameobjects/GameSurface.java
@@ -72,17 +72,19 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
 
     /**
      * Method to get a fitting starting position for the pipe
+     *
      * @return Random number fitted to the screen
      */
     private int calculateBottomY() {
-            return ThreadLocalRandom.current().nextInt(PIPE_GAP+ SCREEN_HEIGHT / 10,SCREEN_HEIGHT - PIPE_GAP);
+        return ThreadLocalRandom.current().nextInt(PIPE_GAP + SCREEN_HEIGHT / 10, SCREEN_HEIGHT - PIPE_GAP);
     }
 
     /**
-     *Initiates the pipes at set start positions
+     * Initiates the pipes at set start positions
+     *
      * @throws IOException
      */
-    public void addPipes() throws IOException{
+    public void addPipes() throws IOException {
         pipes = new ArrayList<>();
         pipes.add(firstPipe = new Pipe(PIPE_WIDTH, PIPE_HEIGHT, SCREEN_WIDTH + PIPE_WIDTH, calculateBottomY(), "top"));
         pipes.add(secondPipe = new Pipe(PIPE_WIDTH, PIPE_HEIGHT, SCREEN_WIDTH + PIPE_WIDTH, firstPipe.getyLoc() - PIPE_GAP - PIPE_HEIGHT, "bot"));
@@ -99,17 +101,15 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
         manageBackground(g);
         if (firstRound) {
             setStartingScreen(g);
+        } else if (gameOver) {
+            setHighScore(g);
+            setGameOverScreen(g);
+        } else {
+            drawPipes(g);
+            drawPoints(g);
+            paintBirb(g);
         }
-        else if (gameOver) {
-                setHighScore(g);
-                setGameOverScreen(g);
-            }
-        else {
-                drawPipes(g);
-                drawPoints(g);
-                paintBirb(g);
-            }
-        }
+    }
 
     private void manageBackground(Graphics g) {
         g.drawImage(background, -backgroundCounter / 2, 0, SCREEN_WIDTH * 2, SCREEN_HEIGHT, this);
@@ -118,8 +118,10 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
             backgroundCounter = 0;
         }
     }
+
     /**
      * Draws the points
+     *
      * @param g
      */
     private void drawPoints(Graphics g) {
@@ -127,8 +129,10 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
         g.setFont(new Font("Candara", Font.BOLD, 90));
         g.drawString(String.valueOf(points), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 9);
     }
+
     /**
      * Animates the pipes
+     *
      * @param g
      */
     private void drawPipes(Graphics g) {
@@ -139,6 +143,7 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
 
     /**
      * Paints the starting screen with instructions
+     *
      * @param g
      */
     private void setStartingScreen(Graphics g) {
@@ -149,7 +154,7 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
         g.drawString("Welcome to Jumpybirb!", SCREEN_WIDTH / 10, SCREEN_HEIGHT / 5);
         g.drawString("Press SPACE to jump.", SCREEN_WIDTH / 10, 2 * SCREEN_HEIGHT / 5);
         g.drawString("Choose difficulty to start", SCREEN_WIDTH / 10, 3 * SCREEN_HEIGHT / 5);
-        g.drawString("1: EASY   2:NORMAL    3:HARD", SCREEN_WIDTH / 10, 4 * SCREEN_HEIGHT/ 5);
+        g.drawString("1: EASY   2:NORMAL    3:HARD", SCREEN_WIDTH / 10, 4 * SCREEN_HEIGHT / 5);
     }
 
     private void setHighScore(Graphics g) {
@@ -158,45 +163,42 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
         if (!highscore.fileNotRead()) {
             String[] highscores = highscore.printHighscore();
             int y = 50;
-            g.drawString("HIGSCORES", SCREEN_WIDTH/2-120, y);
+            g.drawString("HIGSCORES", SCREEN_WIDTH / 2 - 120, y);
             for (int i = 0; i < highscore.getPoints().length; i++) {
                 y += 50;
-                g.drawString(highscores[i*2], SCREEN_WIDTH/14, y);
-                g.drawString(highscores[i*2+1], SCREEN_WIDTH - SCREEN_WIDTH/12, y);
+                g.drawString(highscores[i * 2], SCREEN_WIDTH / 14, y);
+                g.drawString(highscores[i * 2 + 1], SCREEN_WIDTH - SCREEN_WIDTH / 12, y);
             }
-        }
-
-        else {
-            g.drawString(String.format("Although you scored %d fabulous points,", points), 10, SCREEN_HEIGHT/8);
-            g.drawString("the highscores could unfortunately not", 10, (SCREEN_HEIGHT/8)*2);
-            g.drawString("be retrieved!", 10, (SCREEN_HEIGHT/8)*3);
+        } else {
+            g.drawString(String.format("Although you scored %d fabulous points,", points), 10, SCREEN_HEIGHT / 8);
+            g.drawString("the highscores could unfortunately not", 10, (SCREEN_HEIGHT / 8) * 2);
+            g.drawString("be retrieved!", 10, (SCREEN_HEIGHT / 8) * 3);
         }
     }
 
     /**
      * Sets the style and text for the game over notification
-     *
      */
-     private void setGameOverScreen(Graphics g){
+    private void setGameOverScreen(Graphics g) {
         g.drawImage(birbDead, birb.x, birb.y, birb.width, birb.height, this);
         g.setColor(Color.green);
-        g.fillRect(6*SCREEN_WIDTH/10,  2*SCREEN_HEIGHT/10, 3*SCREEN_WIDTH/10, 4*SCREEN_HEIGHT/10);
+        g.fillRect(6 * SCREEN_WIDTH / 10, 2 * SCREEN_HEIGHT / 10, 3 * SCREEN_WIDTH / 10, 4 * SCREEN_HEIGHT / 10);
         g.setColor(Color.black);
         g.setFont(new Font("Arial", Font.BOLD, SCREEN_HEIGHT / 35));
-        g.drawString("GAME OVER!", 13*SCREEN_WIDTH/20, 3 * SCREEN_HEIGHT / 10);
-        g.drawString("Choose difficulty to play again", 6*SCREEN_WIDTH/10, 4 * SCREEN_HEIGHT / 10);
-        g.drawString("1: EASY  2:NORMAL  3:HARD", 6*SCREEN_WIDTH/10, 5 * SCREEN_HEIGHT / 10);
+        g.drawString("GAME OVER!", 13 * SCREEN_WIDTH / 20, 3 * SCREEN_HEIGHT / 10);
+        g.drawString("Choose difficulty to play again", 6 * SCREEN_WIDTH / 10, 4 * SCREEN_HEIGHT / 10);
+        g.drawString("1: EASY  2:NORMAL  3:HARD", 6 * SCREEN_WIDTH / 10, 5 * SCREEN_HEIGHT / 10);
     }
 
     /**
      * animation for the birb
+     *
      * @param g
      */
     private void paintBirb(Graphics g) {
         if (framesAfterJumpCounter < 20 && framesAfterJumpCounter > 3) {
             g.drawImage(birbDown, birb.x, birb.y, birb.width, birb.height, this);
-        }
-        else {
+        } else {
             g.drawImage(birbUp, birb.x, birb.y, birb.width, birb.height, this);
         }
     }
@@ -216,7 +218,7 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
         }
 
         setBirbYPosition(framesAfterJumpCounter);
-        movePipes();
+        handlePipePosition();
 
         //Increase points if pipes move past a point.
         if (firstPipe.getxLoc() == SCREEN_WIDTH / 2 - PIPE_WIDTH || thirdPipe.getxLoc() == SCREEN_WIDTH / 2 - PIPE_WIDTH) {
@@ -286,11 +288,11 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
      * @param keyCode the key code for the key registered in keyEvent()
      */
     public void startGame(int keyCode) {
-                try {
-                    addPipes();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        try {
+            addPipes();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         this.birb = new Rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4, 60, 40);
         if (keyCode == KeyEvent.VK_SPACE
                 || keyCode == KeyEvent.VK_1
@@ -305,6 +307,7 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
 
     /**
      * Sets the difficulty of the game by changing the timer delay which in controls the frame rate.
+     *
      * @param keyCode key code for chosen difficulty. 1=easy, 2=normal, 3=hard.
      */
     public void setDifficulty(int keyCode) {
@@ -336,22 +339,22 @@ public class GameSurface extends JPanel implements ActionListener, KeyListener {
     /**
      * Method that handle the pipes movement and when they shoud be reset
      */
-    private void movePipes() {
+    private void handlePipePosition() {
         if (firstPipe.getxLoc() == SCREEN_WIDTH / 2 - PIPE_WIDTH) {
             thirdPipe.setItAll(SCREEN_WIDTH, calculateBottomY(), PIPE_HEIGHT, PIPE_WIDTH);
             fourthPipe.setItAll(SCREEN_WIDTH, thirdPipe.getyLoc() - PIPE_GAP - PIPE_HEIGHT, PIPE_HEIGHT, PIPE_WIDTH);
         }
-        if (firstPipe.getxLoc() == -PIPE_WIDTH) {
-            firstPipe.setxLoc(SCREEN_WIDTH + PIPE_WIDTH);
-            secondPipe.setxLoc(SCREEN_WIDTH + PIPE_WIDTH);
-            firstPipe.setyLoc(calculateBottomY());
-            secondPipe.setyLoc(firstPipe.getyLoc() - PIPE_GAP - PIPE_HEIGHT);
-        }
-        if (thirdPipe.getxLoc() == -PIPE_WIDTH) {
-            thirdPipe.setxLoc(SCREEN_WIDTH + PIPE_WIDTH);
-            fourthPipe.setxLoc(SCREEN_WIDTH + PIPE_WIDTH);
-            thirdPipe.setyLoc(calculateBottomY());
-            fourthPipe.setyLoc(thirdPipe.getyLoc() - PIPE_GAP - PIPE_HEIGHT);
+        resetPipePosition(firstPipe, secondPipe);
+        resetPipePosition(thirdPipe, fourthPipe);
+
+    }
+
+    private void resetPipePosition(Pipe one, Pipe two) {
+        if (one.getxLoc() == -PIPE_WIDTH) {
+            one.setxLoc(SCREEN_WIDTH + PIPE_WIDTH);
+            two.setxLoc(SCREEN_WIDTH + PIPE_WIDTH);
+            one.setyLoc(calculateBottomY());
+            two.setyLoc(thirdPipe.getyLoc() - PIPE_GAP - PIPE_HEIGHT);
         }
     }
 }
